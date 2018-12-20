4da3a1c5eadb02cf34b869fbf71d91f851f5ef71
贺浩 <745247555@qq.com>
2018-12-20 18:10:11 +0800

    第四次作业

--- 24-homework/src/westos/demo2/Test.java
+++ 24-homework/src/westos/demo2/Test.java
@@ -1,7 +1,21 @@
 package westos.demo2;
 
-public class Test {
-    public static void main(String[] args) {
+import java.sql.*;
 
+public class Test {
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test3","root","root");
+
+        PreparedStatement stmt = conn.prepareStatement("select * from xx_user where username = ? and password = ? ");
+// 2) 给?占位符赋值
+        stmt.setString(1, "laowang");
+        stmt.setString(2, "aaa' or '1'='1"); // 会将整个值当做一个整体，把or当做了值而不是关键字
+// 3) 运行sql
+        ResultSet rs = stmt.executeQuery(); // 会将sql语句以及通过set方法设置的参数值，一起发送给数据库服务器
+        if(rs.next()) {
+            System.out.println("查询到了");
+        } else {
+            System.out.println("没查询到");
+        }
     }
 }
